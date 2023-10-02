1532b1bd322c65b02cd79d6214b76729cde771e0
CWright2022 <61096126+CWright2022@users.noreply.github.com>
2023-10-01 20:00:18 -0400

    working!

--- solve.py
+++ solve.py
@@ -1,3 +1,26 @@
 #solve RITSEC intro to networking (hard) challenge
 #oct 1, 2023
 #by cayden Wright
+import socket
+import re
+
+HOST = "127.0.0.1"
+INITIAL_PORT = 2048
+
+def main():
+    output=""
+    port = INITIAL_PORT
+    while(True):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((HOST, port))
+        output = s.recv(4096).decode()
+        if(re.match("RS{.*}", output)):
+            print("FOUND FLAG: "+output)
+            break
+        print(output)
+        port = re.findall("\d{5}", output)[0]
+        print("going to port: "+port)
+        port=int(port)
+        s.close()
+if __name__=="__main__":
+    main()
